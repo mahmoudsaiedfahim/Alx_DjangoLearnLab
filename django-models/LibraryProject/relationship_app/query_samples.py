6c2f9a0bd8d0f0f67889843f67a333363ebc07d4
Mahmoud Saied <mahmoudsaiedfahim@gmail.com>
2024-11-14 08:58:00 +0200

    django-models

--- django-models/LibraryProject/relationship_app/query_samples.py
+++ django-models/LibraryProject/relationship_app/query_samples.py
@@ -8,4 +8,4 @@ library_name = ''
 books = Library.objects.get(name=library_name)
 all_books_library = books.all()
 library_instance = Library.objects.get(id=1)
-librarian = Librarian.objects.filter(library=library_instance)
+librarian = Librarian.objects.get(library=library_instance)
