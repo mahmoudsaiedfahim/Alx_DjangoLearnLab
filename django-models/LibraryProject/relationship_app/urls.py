39cdf84bddfba8299d6585465a1ac9160b9a0816
Mahmoud Saied <mahmoudsaiedfahim@gmail.com>
2024-11-16 18:31:13 +0200

    django-models-2

--- django-models/LibraryProject/relationship_app/urls.py
+++ django-models/LibraryProject/relationship_app/urls.py
@@ -2,8 +2,12 @@ from django.urls import path
 from . import views
 from .views import list_books
 from .views import LibraryDetailView
+from django.contrib.auth.views import LoginView
+from django.contrib.auth.views import LogoutView
 
 urlpatterns = [
     path('books/', list_books, name='list_books'),
     path('library/<int:pk/>', LibraryDetailView.as_view(), name='library_detail'),
+    path('login/', LoginView.as_view(template_name='relationship_app/templates/register.html'), name='login'),
+    path('logout', LogoutView.as_view(), name= 'logout'),
 ]
--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -5,12 +5,18 @@ from django.views.generic import ListView, DetailView, TemplateView
 from .models import Library
 from django.views.generic.detail import DetailView
 from django.contrib.auth.models import User
+from django.contrib.auth.forms import UserCreationForm
+from django.urls import reverse_lazy
+from django.views.generic import CreateView
+
+
 
 # Create your views here.
 def list_books(request):
     books = Book.objects.all()
     context = {'book_list':books}
     return render(request,'relationship_app/list_books.html',context)
+
 
 class LibraryDetailView(DetailView):
     model = Library
@@ -21,8 +27,17 @@ class LibraryDetailView(DetailView):
         library = self.get_object()
         context['books'] = self.object.get.all()
 
-# Creat an new user
+'''# Creat an new user
 user = User.objects.create_user('mahmoud', 'mahmoud@example.com', '1234')
 
 # Retrieve a user based on a username
-user = user.objects.get(username= 'mahmoud')
+user = user.objects.get(username= 'mahmoud')'''
+
+class RegisterView(CreateView):
+    form_class = UserCreationForm
+    success_url = reverse_lazy('login')
+    template_name = 'relationship_app/templates/register.html'
+
+class LoginView(LoginView):
+    form_class = use
+
