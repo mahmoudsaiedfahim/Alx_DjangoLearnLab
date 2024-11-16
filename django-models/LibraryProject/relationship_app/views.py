8344fa10154005a1743d81ced11444e70e6963d7
Mahmoud Saied <mahmoudsaiedfahim@gmail.com>
2024-11-16 18:48:04 +0200

    django-models-2

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -8,6 +8,8 @@ from django.contrib.auth.models import User
 from django.contrib.auth.forms import UserCreationForm
 from django.urls import reverse_lazy
 from django.views.generic import CreateView
+from django.contrib.auth import login
+from django.contrib.auth import logout
 
 
 
