ee8e6cb0505b1dc06bf07574f2794d37d50732dd
Mahmoud Saied <mahmoudsaiedfahim@gmail.com>
2024-11-16 19:00:29 +0200

    django-models-2

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -36,10 +36,9 @@ user = User.objects.create_user('mahmoud', 'mahmoud@example.com', '1234')
 user = user.objects.get(username= 'mahmoud')'''
 
 class RegisterView(CreateView):
-    form_class = UserCreationForm
+    form_class = UserCreationForm()
     success_url = reverse_lazy('login')
-    template_name = 'relationship_app/templates/register.html'
+    template_name = 'relationship_app/templates/relationship_app/register.html'
 
-class LoginView(LoginView):
-    form_class = use
+
 
