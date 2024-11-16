9fcf06ca5af5541f096499acf5bc4a7ba83fca37
Mahmoud Saied <mahmoudsaiedfahim@gmail.com>
2024-11-16 18:00:14 +0200

    django-models-1

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -4,6 +4,7 @@ from .models import Author, Book, Library, Librarian
 from django.views.generic import ListView, DetailView, TemplateView
 from .models import Library
 from django.views.generic.detail import DetailView
+from django.contrib.auth.models import User
 
 # Create your views here.
 def list_books(request):
@@ -19,3 +20,9 @@ class LibraryDetailView(DetailView):
         context = super().get_context_data(**kwargs)
         library = self.get_object()
         context['books'] = self.object.get.all()
+
+# Creat an new user
+user = User.objects.create_user('mahmoud', 'mahmoud@example.com', '1234')
+
+# Retrieve a user based on a username
+user = user.objects.get(username= 'mahmoud')
