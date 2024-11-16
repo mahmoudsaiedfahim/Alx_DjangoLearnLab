5115dc9c3bb75cd2736073f53ac248b49d4e64f2
Mahmoud Saied <mahmoudsaiedfahim@gmail.com>
2024-11-16 21:44:31 +0200

    django-models-3

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -10,8 +10,8 @@ from django.urls import reverse_lazy
 from django.views.generic import CreateView
 from django.contrib.auth import login
 from django.contrib.auth import logout
-
-
+from django.contrib.auth.decorators import user_passes_test
+from django.contrib.auth.decorators import login_required
 
 # Create your views here.
 def list_books(request):
@@ -36,9 +36,27 @@ user = User.objects.create_user('mahmoud', 'mahmoud@example.com', '1234')
 user = user.objects.get(username= 'mahmoud')'''
 
 class RegisterView(CreateView):
-    form_class = UserCreationForm()
+    form_class = UserCreationForm
     success_url = reverse_lazy('login')
     template_name = 'relationship_app/templates/relationship_app/register.html'
 
+
+def is_admin(user):
+    return user.userprofile.role == 'Admin'
+@user_passes_test(is_admin)
+def admin_view(request):
+    return render(request, 'relationship_app/templates/relationship_app/admin_view.html')
+
+def is_librarian(user):
+    return user.userprofile.role == 'Librarian'
+@user_passes_test(is_librarian)
+def librarian_view(request):
+    return render(request, 'relationship_app/templates/relationship_app/librarian_view.html')
+
+def is_member(user):
+    return user.userprofile.role == 'Member'
+@user_passes_test(is_member)
+def member_view(request):
+    return render(request, 'relationship_app/templates/relationship_app/member_view.html')
 
 
