e337a1378cdbbe4e1cb0344a4db1471a316198d7
Mahmoud Saied <mahmoudsaiedfahim@gmail.com>
2024-12-01 13:07:05 +0200

    advanced-api-project-1

--- advanced-api-project/api/views.py
+++ advanced-api-project/api/views.py
@@ -3,8 +3,7 @@ from django.shortcuts import render
 from rest_framework import generics
 from .models import Book, Author
 from .serializers import BookSerializer, AuthorSerializer
-from rest_framework.permissions import IsAuthenticated
-from rest_framework.permissions import IsAuthenticatedOrReadOnly
+from rest_framework.permissions import IsAuthenticatedOrReadOnly, IsAuthenticated
 # Create your views here.
 class CustomBookListView(generics.ListAPIView):
     queryset = Book.objects.all()
