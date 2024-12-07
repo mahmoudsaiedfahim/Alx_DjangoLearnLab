8dbcb5f9dfa7afc21b9ceb2c03575bbe93476b2d
Mahmoud Saied <mahmoudsaiedfahim@gmail.com>
2024-12-07 21:33:51 +0200

    django_blog-2

--- django_blog/blog/urls.py
+++ django_blog/blog/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path # type: ignore
 from django.contrib.auth import views as auth_views
-from .views import register, HomeView, PostsView, ProfileView
+from .views import register, HomeView, ProfileView
+from .views import PostListView, PostDetailView, PostCreateView, PostUpdateView, PostDeleteView
 
 urlpatterns = [
 
@@ -9,6 +10,11 @@ urlpatterns = [
     path('logout/',auth_views.LogoutView.as_view(template_name='blog/logout.html'), name='logout'),
     path('register/',register, name='register'),
     path('profile/', ProfileView.as_view(template_name='blog/profile.html'), name='profile'),
-    path('posts/', PostsView.as_view(template_name='blog/posts.html'), name='posts'),
+    #path('posts/', PostsView.as_view(template_name='blog/posts.html'), name='posts'),
 
+    path('posts/', PostListView.as_view(template_name='blog/post_list.html'), name='post-list'),  # List all posts
+    path('post/<int:pk>/', PostDetailView.as_view(template_name='blog/post_detail.html'), name='post-detail'),  # Detail view for a single post
+    path('post/new/', PostCreateView.as_view(template_name='blog/post_create.html'), name='post-create'),  # Create a new post
+    path('post/<int:pk>/edit/', PostUpdateView.as_view(template_name='blog/post_update.html'), name='post-update'),  # Edit an existing post
+    path('post/<int:pk>/delete/', PostDeleteView.as_view(template_name='blog/post_delete.html'), name='post-delete'),  # Delete a post
 ]
--- django_blog/blog/views.py
+++ django_blog/blog/views.py
@@ -1,14 +1,17 @@
 from django.shortcuts import render, redirect
 from .forms import CustomUserCreationForm
-from django.views.generic import TemplateView, ListView
+from django.views.generic import TemplateView, ListView, DetailView, CreateView, UpdateView, DeleteView
 from .models import Post
+from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
+from django.urls import reverse_lazy
 
 # Create your views here.
 class ProfileView(TemplateView):
     template_name = 'profile.html'
-class PostsView(ListView):
+
+'''class PostsView(ListView):
     model = Post
-    template_name = 'posts.html'
+    template_name = 'posts.html'''
 
 class HomeView(TemplateView):
     template_name = 'home.html'
@@ -24,4 +27,46 @@ def register(request):
         form = CustomUserCreationForm()
     return render(request, 'blog/register.html', {'form': form})
 
+class PostListView(ListView):
+    model = Post
+    template_name = 'post_list.html'
+    context_object_name = 'posts'
 
+class PostDetailView(DetailView):
+    model = Post
+    template_name = 'post_detail.html'
+    context_object_name = 'post'
+
+class PostCreateView(LoginRequiredMixin, CreateView):
+    model = Post
+    template_name = 'post_create'
+    fields = ['title', 'content']
+    success_url = reverse_lazy('post-list')
+
+    def form_valid(self, form):
+        form.instance.author = self.request.user  # Set the author to the current user
+        return super().form_valid(form)
+
+
+class PostUpdateView(LoginRequiredMixin, UserPassesTestMixin, UpdateView):
+    model = Post
+    template_name = 'post_update'
+    fields = ['title', 'content']
+    success_url = reverse_lazy('post-list')
+
+    def form_valid(self, form):
+        form.instance.author = self.request.user
+        return super().form_valid(form)
+
+    def test_func(self):
+        post = self.get_object()
+        return self.request.user == post.author  # Allow only the author to edit
+    
+class PostDeleteView(LoginRequiredMixin, UserPassesTestMixin, DeleteView):
+    model = Post
+    template_name = 'post_delete'
+    success_url = reverse_lazy('post-list')
+
+    def test_func(self):
+        post = self.get_object()
+        return self.request.user == post.author  # Allow only the author to delete
