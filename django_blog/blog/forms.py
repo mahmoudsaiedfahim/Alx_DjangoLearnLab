36e74699d950c5f362d5dcd4b1a8a1de16079766
Mahmoud Saied <mahmoudsaiedfahim@gmail.com>
2024-12-08 16:52:33 +0200

    django_blog-4

--- django_blog/blog/forms.py
+++ django_blog/blog/forms.py
@@ -2,6 +2,7 @@ from django import forms
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
 from .models import Post, Comment
+from taggit.forms import TagWidget
 
 class CustomUserCreationForm(UserCreationForm):
     email = forms.EmailField(required=True)
@@ -14,4 +15,14 @@ class CommentForm(forms.ModelForm):
     class Meta:
         model = Comment
         fields = ['content']
-        
+
+class PostCreateForm(forms.ModelForm):
+     title = forms.CharField(max_length=100)
+     content = forms.Textarea()
+     
+     class Meta:
+          model = Post
+          fields = ['title', 'content', 'tags'] 
+          widgets = {
+            'tags': TagWidget(),
+          }
