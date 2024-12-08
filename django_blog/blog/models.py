649517c352eb7b1b9cf6703cca5382bd5391e8da
Mahmoud Saied <mahmoudsaiedfahim@gmail.com>
2024-12-08 16:38:21 +0200

    django_blog-4

--- django_blog/blog/models.py
+++ django_blog/blog/models.py
@@ -1,29 +1,26 @@
 from django.db import models
 #from django.contrib.auth.models import User
 from django.contrib.auth.models import User
+from taggit.managers import TaggableManager  # Import TaggableManager
+
 
 # Create your models here.
 class Post(models.Model):
-
     title = models.CharField(max_length=200)
     content = models.TextField()
     published_date = models.DateTimeField(auto_now_add=True)
     author = models.ForeignKey(User, on_delete=models.CASCADE)
-    tags = models.ManyToManyField('Tag', related_name='posts', blank=True)  # Many-to-many relationship
-    def __str__(self):
-        return self.title
-    
-class Tag(models.Model):
-    name = models.CharField(max_length=50, unique=True)
+    tags = TaggableManager(blank=True)  # Use TaggableManager for tags
 
     def __str__(self):
-        return self.name
-    
+        return self.title
+
 class Comment(models.Model):
     post = models.ForeignKey(Post, on_delete=models.CASCADE)
     author = models.ForeignKey(User, on_delete=models.PROTECT)
     content = models.TextField(max_length=500)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
+
     def __str__(self):
         return f'Comment by {self.author.username} on {self.post.title}'
--- django_blog/django_blog/settings.py
+++ django_blog/django_blog/settings.py
@@ -32,6 +32,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'taggit',
     'rest_framework',
     'blog',
     'django.contrib.admin',
