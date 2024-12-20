464653b25ba5219c447eeb85a56ea9d9f7c95455
Mahmoud Saied <mahmoudsaiedfahim@gmail.com>
2024-12-20 14:53:40 +0200

    social_media_api_2

--- social_media_api/accounts/urls.py
+++ social_media_api/accounts/urls.py
@@ -1,7 +1,10 @@
 
 from django.urls import path, include
-from .views import UserRegistrationView, CustomAuthToken
+from .views import UserRegistrationView, CustomAuthToken, FollowUserView, UnfollowUserView
 urlpatterns = [
     path('register/', UserRegistrationView.as_view(), name='register'),
     path('login/', CustomAuthToken.as_view(), name='login'),
+    path('follow/<int:user_id>/', FollowUserView.as_view(), name='follow-user'),
+    path('unfollow/<int:user_id>/', UnfollowUserView.as_view(), name='unfollow-user'),
+
 ]
