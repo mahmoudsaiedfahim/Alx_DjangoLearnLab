500dd8b946bf39aec6be7e71dbc89eb706529543
Mahmoud Saied <mahmoudsaiedfahim@gmail.com>
2024-12-20 16:41:52 +0200

    social_media_api_4

--- social_media_api/social_media_api/settings.py
+++ social_media_api/social_media_api/settings.py
@@ -80,10 +80,24 @@ WSGI_APPLICATION = 'social_media_api.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
-DATABASES = {
+'''DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': BASE_DIR / 'db.sqlite3',
+    }
+
+}'''
+
+
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'your_db_name',
+        'USER': 'your_db_user',
+        'PASSWORD': 'your_db_password',
+        'HOST': 'your_db_host',
+        'PORT': 'your_db_port',
     }
 }
 
