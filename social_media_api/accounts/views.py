c0d527f307a62bb5793d00217b94bc1c853af914
Mahmoud Saied <mahmoudsaiedfahim@gmail.com>
2024-12-20 14:48:32 +0200

    social_media_api_1

--- social_media_api/accounts/views.py
+++ social_media_api/accounts/views.py
@@ -1,11 +1,13 @@
 from django.shortcuts import render
-from rest_framework import generics, permissions
+from rest_framework import generics, permissions, views, status
 from rest_framework.authtoken.views import ObtainAuthToken
 from rest_framework.authtoken.models import Token
 from rest_framework.response import Response
 from .serializers import CustomUserSerializer, LoginSerializer
 #from django.contrib.auth.cmodels import User
 from django.contrib.auth import get_user_model
+from .models import CustomUser
+from notifications.models import Notification
 
 User = get_user_model()
 # Create your views here.
@@ -22,3 +24,25 @@ class CustomAuthToken(ObtainAuthToken):
         token, created = Token.objects.get_or_create(user=user)
         return Response({'token':token.key, 'user_id': user.pk})
     
+class FollowUserView(generics.GenericAPIView):
+    permission_classes = [permissions.IsAuthenticated]
+
+    def post(self, request, user_id):
+        try:
+            user_to_follow = CustomUser.objects.get(id=user_id)
+            if request.user != user_to_follow:
+                request.user.following.add(user_to_follow)
+                Notification.objects.create(
+                    recipient=user_to_follow,
+                    actor=request.user,
+                    verb='started following you',
+                    target=user_to_follow
+                )
+                return Response({'message': f'You are now following {user_to_follow.username}'}, status=status.HTTP_200_OK)
+            else:
+                return Response({'message': 'You cannot follow yourself'}, status=status.HTTP_400_BAD_REQUEST)
+        except CustomUser.DoesNotExist:
+            return Response({'message': 'User not found'}, status=status.HTTP_404_NOT_FOUND)
+
+    
+
